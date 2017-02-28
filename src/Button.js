edebd27eb5ac132b03e6fabbe3ec33b258587a35
Dave van Bale <dave.van.bale@gmail.com>
2017-02-28 16:19:30 +0100

    Initial commit of project

---
+++ src/Button.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Button extends Component {
+    render() {
+        return (
+            <button>test</button>
+        )
+    }
+}
+
+export default Button;
